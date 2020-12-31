9af8e0b9fdd6439adb3b1490722ca379f55f37c3
Ricki <rickipeikes@gmail.com>
2020-12-31 17:38:43 -0600

    added accesors to Point and basic methods to analyzer

--- src/main/java/draw/shapes/ShapeAnalyzer.java
+++ src/main/java/draw/shapes/ShapeAnalyzer.java
@@ -1,6 +1,39 @@
 package draw.shapes;
 
+import java.util.List;
+
 //Determine which shape- if any
 //Ricki
 public class ShapeAnalyzer {
+    //private List<Point> stroke;
+    private final int ERROR_ALLOWANCE = 5;
+
+    public boolean isHorizontal(List<Point> stroke) {
+        Point origin = stroke.get(0);
+
+        for (Point point : stroke) {
+            if (veersVertical(point, origin)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean veersVertical(Point point, Point origin) {
+        return point.getY() > origin.getY() + ERROR_ALLOWANCE || point.getY() < origin.getY() - ERROR_ALLOWANCE;
+    }
+
+    public boolean isVertical(List<Point> stroke) {
+        Point origin = stroke.get(0);
+        for (Point point : stroke) {
+            if (veersHorizontal(point, origin)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean veersHorizontal(Point point, Point origin) {
+        return point.getX() > origin.getX() + ERROR_ALLOWANCE || point.getX() < origin.getX() - ERROR_ALLOWANCE;
+    }
 }
