2ebc3ac8e8755a2c80efd263355ab41fcf539a59
Ricki <rickipeikes@gmail.com>
2020-12-31 20:28:14 -0600

    finished vertical test

--- src/main/java/draw/shapes/ShapeAnalyzer.java
+++ src/main/java/draw/shapes/ShapeAnalyzer.java
@@ -24,10 +24,46 @@ public class ShapeAnalyzer {
     }
 
     public boolean isVertical(List<Point> stroke) {
+
+        boolean topDown = stroke.get(0).getY() < stroke.get(1).getY();
+        if (topDown) {
+            return isTopDownLine(stroke);
+        } else {
+            return isBottomUpLine(stroke);
+        }
+
+    }
+
+    private boolean isBottomUpLine(List<Point> stroke) {
         Point origin = stroke.get(0);
-        for (Point point : stroke) {
-            if (veersHorizontal(point, origin)) {
+        int minY = 0;
+        for (int i = 1; i < stroke.size(); i++) {
+
+            if (veersHorizontal(stroke.get(i), origin)) {
                 return false;
+            } else {
+                if (stroke.get(i).getY() > minY) {
+                    return false;
+                } else {
+                    minY = stroke.get(i).getY();
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isTopDownLine(List<Point> stroke) {
+        Point origin = stroke.get(0);
+        int maxY = 0;
+        for (int i = 1; i < stroke.size(); i++) {
+            if (veersHorizontal(stroke.get(i), origin)) {
+                return false;
+            }
+
+            if (stroke.get(i).getY() < maxY) {
+                return false;
+            } else {
+                maxY = stroke.get(i).getY();
             }
         }
         return true;
@@ -36,4 +72,6 @@ public class ShapeAnalyzer {
     private boolean veersHorizontal(Point point, Point origin) {
         return point.getX() > origin.getX() + ERROR_ALLOWANCE || point.getX() < origin.getX() - ERROR_ALLOWANCE;
     }
+
+
 }
