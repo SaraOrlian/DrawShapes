53429ab2c62b80b82b987224c37fb42226d41659
Ricki <rickipeikes@gmail.com>
2021-01-01 11:01:05 -0600

    trying to listen to strokes

--- src/main/java/draw/shapes/DrawShapesFrame.java
+++ src/main/java/draw/shapes/DrawShapesFrame.java
@@ -2,15 +2,25 @@ package draw.shapes;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 //Sara
 public class DrawShapesFrame extends JFrame {
+
+
+    MouseListener listener = new MouseListener();
+
 
     public DrawShapesFrame(ShapesView shapesView) {
         setSize(500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Draw Shapes");
         setLayout(new BorderLayout());
+        JLabel lineType = listener.lineType;
+        lineType.setText("blank");
+        add(lineType);
+
+
     }
 
 }
--- src/main/java/draw/shapes/MouseListener.java
+++ src/main/java/draw/shapes/MouseListener.java
@@ -1,5 +1,6 @@
 package draw.shapes;
 
+import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -7,17 +8,24 @@ import java.util.List;
 
 //Shana
 public class MouseListener extends MouseInputAdapter {
-
-    List<Point> stroke = new ArrayList<>();
+    ShapeAnalyzer analyzer = new ShapeAnalyzer();
+    private List<Point> currentStroke = new ArrayList<>();
+    //List<List<Point>> strokes = new ArrayList<>();
+    JLabel lineType = new JLabel();
 
     public void mouseDragged(MouseEvent e) {
+
         Point point = new draw.shapes.Point(e.getX(), e.getY());
-        stroke.add(point);
+        currentStroke.add(point);
     }
 
     public void mouseReleased(MouseEvent e) {
-        //return array
-        stroke.clear();
+        if (analyzer.isHorizontal(currentStroke)) {
+            lineType.setText("Horizontal");
+        } else {
+            lineType.setText("Invalid");
+        }
+        currentStroke.clear();
     }
 
 
@@ -25,4 +33,7 @@ public class MouseListener extends MouseInputAdapter {
     public void mouseMoved(MouseEvent e) {
 
     }
+
+
+
 }
