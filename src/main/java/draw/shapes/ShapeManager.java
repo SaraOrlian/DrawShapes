87711fe7310b1eb5fcbb5fb3c1b6cefd9daf42bf
SaraOrlian <orlian.sara@gmail.com>
2021-01-02 22:15:29 -0500

    assigned strings to Shape enum, and cleaned up view to adapt to the changes made during previous meeting

--- src/main/java/draw/shapes/ShapeManager.java
+++ src/main/java/draw/shapes/ShapeManager.java
@@ -1,36 +1,7 @@
 package draw.shapes;
 
-<<<<<<< HEAD
+
 import java.text.Format;
-import java.util.LinkedList;
-
-//take care of adding and removing to queue
-public class ShapeManager {
-
-    private LinkedList<String> shapeList1 = new LinkedList<>();
-    private final String horizontal = "\u2796";
-    private final String vertical = "|";
-    private final String ascii = "\u2796";
-
-    public ShapeManager(){
-        setupList(shapeList1);
-    }
-
-    private void setupList(LinkedList<String> shapeList1) {
-        shapeList1.add(horizontal);
-        shapeList1.add(horizontal);
-        shapeList1.add(vertical);
-        shapeList1.add(vertical);
-        shapeList1.add(ascii);
-        shapeList1.add(horizontal);
-        shapeList1.add(horizontal);
-        shapeList1.add(vertical);
-    }
-
-    public LinkedList<String> getShapeList1() {
-        return shapeList1;
-    }
-=======
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -38,8 +9,7 @@ import java.util.Queue;
 
 //take care of adding and removing to queue
 public class ShapeManager {
-    List<Queue<Shape>> shapeQueues = new ArrayList<>();
 
->>>>>>> 12133e3598022045d07c6acc867e8f574348ba3c
+    List<Queue<Shape>> shapeQueues = new ArrayList<>();
 
 }
--- src/main/java/draw/shapes/ShapesView.java
+++ src/main/java/draw/shapes/ShapesView.java
@@ -1,55 +1,67 @@
 package draw.shapes;
 
-<<<<<<< HEAD
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.Queue;
 import java.util.Random;
 
-//Sara
 public class ShapesView extends JComponent {
 
     private ShapeManager shapeManager = new ShapeManager();
-    private LinkedList<String> list = shapeManager.getShapeList1();
+    private Queue<Shape> list; //not set to anything- where are the lists kept?
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawList(g, list);
+        drawList(g);
     }
 
-    private void drawList(Graphics g, LinkedList<String> list) {
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("",Font.PLAIN,30));
+    private void drawList(Graphics g) {
+        g.setFont(new Font("", Font.PLAIN, 30));
+        //Generate random coords for displaying list randomly
         Random rand = new Random();
         int randX = rand.nextInt(400);
         int randY = rand.nextInt(400);
-        String listVal;
-        for (int i = 0; i<list.size(); i++) {
-            listVal = list.get(i);
-            if (listVal == "|") {
-                g.setColor(Color.BLUE);
-                g.drawString("\u2796", randX+(i), randY);
-                randX+=50;
-            } else if (listVal == "\u2796") {
-                g.setColor(Color.RED);
-                randX+=10;
-                Graphics2D g2d = (Graphics2D)g;
-                AffineTransform orig = g2d.getTransform();
-                g2d.rotate(Math.toRadians(-90), randX+(i), randY);
-                g2d.drawString(listVal, randX+(i), randY);
-                g2d.setTransform(orig);
-                randX+=10;
-                g.setColor(Color.ORANGE);
-                g.drawString("\u26AA", randX+(i), randY);
-                randX+=40;
+        //iterate through queue and display list
+        Iterator iterator = list.iterator();
+        Shape listVal;
+        while (iterator.hasNext()) {
+            listVal = (Shape) iterator.next();
+            switch (listVal) {
+                case UP_SLOPE -> {
+                    g.setColor(Color.ORANGE);
+                    g.drawString(listVal.toString(), randX++, randY);
+                    break;
+                }
+                case DOWN_SLOPE -> {
+                    g.setColor(Color.MAGENTA);
+                    g.drawString(listVal.toString(), randX++, randY);
+                    break;
+                }
+                case HORIZONTAL -> {
+                    g.setColor(Color.RED);
+                    randX += 10;
+                    Graphics2D g2d = (Graphics2D) g;
+                    AffineTransform orig = g2d.getTransform();
+                    g2d.rotate(Math.toRadians(-90), randX++, randY);
+                    g2d.drawString(listVal.toString(), randX++, randY);
+                    g2d.setTransform(orig);
+                    randX += 10;
+                    break;
+                }
+                case VERTICAL -> {
+                    g.setColor(Color.BLUE);
+                    g.drawString(listVal.toString(), randX++, randY);
+                    randX += 50;
+                    break;
+                }
+                default -> {
+                    return;
+                }
             }
-
-
         }
     }
-=======
-public class ShapesView {
->>>>>>> 12133e3598022045d07c6acc867e8f574348ba3c
 }
