12588f4dac1bb97f51c67402830724386f0187e3
Ricki <rickipeikes@gmail.com>
2021-01-03 23:20:11 -0600

    increased accuraccy of isV method

--- src/main/java/draw/shapes/ShapeAnalyzer.java
+++ src/main/java/draw/shapes/ShapeAnalyzer.java
@@ -8,15 +8,9 @@ public class ShapeAnalyzer {
 
     private final double HORIZONTAL_ERROR_ALLOWANCE = 5;
     private final double V_ERROR_ALLOWANCE = 10;
-
-    private final double MAX_POS_SLOPE = 3;
     private final double MIN_POS_SLOPE = 0.47;
     private final double MAX_NEG_SLOPE = -0.42;
-    private final double MIN_NEG_SLOPE = -3;
 
-    //smooth out line by reducing little lines onto one large line where slopes are all similar, so
-    // that you only look at the points that define the shape
-    //1- draw visually 2- reduce 3- test with perfect coordinates
     public boolean isHorizontal(List<Point> stroke) {
         Point start = stroke.get(0);
         Point end = stroke.get(stroke.size() - 1);
@@ -59,21 +53,34 @@ public class ShapeAnalyzer {
         }
         Point vertex = null;
         Point previousPoint = start;
+        int vertexIndex = 0;
         double prevSlope = 0;
-        for (Point point : stroke) {
-            double currSlope = calcSlope(point, previousPoint);
+        for (int i = 0; i < stroke.size(); i++) {
+            double currSlope = calcSlope(stroke.get(i), previousPoint);
             if (currSlope > 0) {
                 vertex = previousPoint;
+                vertexIndex = i - 1;
                 break;
             }
             if (currSlope - prevSlope > V_ERROR_ALLOWANCE) {
                 return false;
             }
             prevSlope = currSlope;
-            previousPoint = point;
+            previousPoint = stroke.get(i);
         }
         if (vertex == null) {
             return false;
+        }
+        for (int i = vertexIndex; i < stroke.size() - 1; i++) {
+            double currSlope = calcSlope(stroke.get(i), previousPoint);
+            if (currSlope < 0) {
+                return false;
+            }
+            if (currSlope - prevSlope > V_ERROR_ALLOWANCE) {
+                return false;
+            }
+            prevSlope = currSlope;
+            previousPoint = stroke.get(i);
         }
 
         return true;
@@ -108,23 +115,9 @@ public class ShapeAnalyzer {
     }
 
 
-
     private boolean isInZeroRange(double slope) {
         return slope > MAX_NEG_SLOPE && slope < MIN_POS_SLOPE;
     }
-
-    private boolean isInPosRange(double slope) {
-        return slope > MIN_POS_SLOPE && slope < MAX_POS_SLOPE;
-    }
-
-    private boolean isInNegRange(double slope) {
-        return slope > MIN_NEG_SLOPE && slope < MAX_NEG_SLOPE;
-    }
-
-    private boolean isInUndefRange(double slope) {
-        return slope > MAX_POS_SLOPE || slope < MAX_NEG_SLOPE;
-    }
-
 
     private double calcSlope(Point point1, Point point2) {
         return -1 * (point1.getY() - point2.getY()) / (double) (point1.getX() - point2.getX());
