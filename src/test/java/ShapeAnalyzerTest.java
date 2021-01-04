33cc365f45c71535b7b9cf90cdb6f3e2e5a79c84
Ricki <rickipeikes@gmail.com>
2021-01-04 11:24:56 -0600

    fixed v test

--- src/test/java/ShapeAnalyzerTest.java
+++ src/test/java/ShapeAnalyzerTest.java
@@ -14,16 +14,24 @@ public class ShapeAnalyzerTest {
 //given
         ShapeAnalyzer analyzer = new ShapeAnalyzer();
         List<Point> stroke = new ArrayList<Point>();
-        int peakX = 4;
-        int num = 3;
+        int slope = 1;
         //when
-        stroke.add(new Point(10,50));
-        stroke.add(new Point(20,60));
-        stroke.add(new Point(30,70));
-        stroke.add(new Point(40 ,80)); //vertex
-        stroke.add(new Point(50,70));
-        stroke.add(new Point(60,60));
-        stroke.add(new Point( 70,50));
+        for (int i = 0; i < 40; i++) {
+            stroke.add(new Point(i, i + 40));
+        }
+//        stroke.add(new Point(10,50));
+//        stroke.add(new Point(20,60));
+//        stroke.add(new Point(30,70));
+        stroke.add(new Point(40, 80)); //vertex
+
+        int num = 79;
+        for (int i = 41; i < 70; i++) {
+            stroke.add(new Point(i, num));
+            num--;
+        }
+//        stroke.add(new Point(50, 70));
+//        stroke.add(new Point(60, 60));
+//        stroke.add(new Point(70, 50));
 
         //then
         assertTrue(analyzer.isV(stroke));
@@ -68,7 +76,7 @@ public class ShapeAnalyzerTest {
         int peakX = 4;
         int num = 5;
         //when
-        for (int i = 0; i < peakX-1; i++) {
+        for (int i = 0; i < peakX - 1; i++) {
             Point temp = new Point(1, num);
             stroke.add(i, temp);
             num--;
