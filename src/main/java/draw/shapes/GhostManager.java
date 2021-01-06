47a790dd80c627d29f0b1c043c0c31a1cfc951fa
shana <shanaw1999@gmail.com>
2021-01-05 21:43:26 -0500

    change for loop to iterator

--- src/main/java/draw/shapes/GhostManager.java
+++ src/main/java/draw/shapes/GhostManager.java
@@ -1,10 +1,7 @@
 package draw.shapes;
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 //Get shape drawn and remove shape from ghost
 public class GhostManager {
@@ -29,11 +26,15 @@ public class GhostManager {
     }
 
     public void dequeueShape() {
-        //iterator so we can remove from list as we go thru it
-        for (Queue<Shape> ghost : ghostList) {
-            Shape drawing = analyzer.strokeIsShape();
-            if (ghost.peek() != null && ghost.peek() == drawing) {
+        Shape drawing = analyzer.strokeIsShape();
+        Iterator<Queue<Shape>> iterator = ghostList.iterator();
+        while(iterator.hasNext()) {
+            Queue<Shape> ghost = iterator.next();
+            if (ghost.peek() == drawing){
                 ghost.remove();
+            }
+            if(ghost.isEmpty()) {
+                iterator.remove();
             }
         }
     }
