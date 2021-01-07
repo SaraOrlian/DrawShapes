96a4a2c1288f566e60328c9d43e1de0ef167961f
shana <shanaw1999@gmail.com>
2021-01-06 20:38:31 -0500

    enhance thread

--- src/main/java/draw/shapes/GhostThread.java
+++ src/main/java/draw/shapes/GhostThread.java
@@ -5,6 +5,7 @@ public class GhostThread extends Thread{
     private int delay = 5000;
     private int counter;
     private int numShapes = 2;
+    private int numGhosts = 1;
     private final GhostManager ghostManager;
     private final ShapesView shapesView;
 
@@ -20,11 +21,17 @@ public class GhostThread extends Thread{
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            ghostManager.createGhost(numShapes);
+            for(int i = 0; i < numGhosts; i++) {
+                ghostManager.createGhost(numShapes);
+            }
             counter++;
-            if(counter%5 == 0) {
-                numShapes++;
+            if(counter%20 == 0) {
+                numGhosts++;
                 delay -=100;
+            }
+            if(counter%10 == 0) {
+                numShapes++;
+                delay -=50;
             }
             shapesView.repaint();
         }
