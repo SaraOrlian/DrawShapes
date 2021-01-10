0a9700c27c7a3b53a89eccfa02b008fbfd222751
shana <shanaw1999@gmail.com>
2021-01-10 15:40:31 -0500

    cleanup

--- src/main/java/draw/shapes/StrokeListener.java
+++ src/main/java/draw/shapes/StrokeListener.java
@@ -21,7 +21,6 @@ public class StrokeListener extends MouseInputAdapter {
     public void mouseDragged(MouseEvent e) {
         Point point = new draw.shapes.Point((int) e.getPoint().getX(), (int) e.getPoint().getY());
         stroke.add(point);
-        //System.out.println(point);
         shapesView.addPoint(point);
         e.getComponent().repaint();
     }
@@ -33,6 +32,5 @@ public class StrokeListener extends MouseInputAdapter {
         stroke.clear();
         shapesView.clearDrawing();
         e.getComponent().repaint();
-        //shapeAnalyzer.whichStroke();
     }
 }
